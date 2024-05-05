443d0797391c355857ac51cc187aa43ebe17680f
Юнусов Вадим <101979218+fl0wka@users.noreply.github.com>
2024-05-05 22:06:31 +0200

    some fixed

--- server/route.js
+++ server/route.js
@@ -1,8 +1,18 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.send('Ответ с сервера');
-});
+	// для CORS
+	res.setHeader('Access-Control-Allow-Origin', '*')
 
-module.exports = router;
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+	)
+
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+
+	res.send('Ответ с сервера')
+})
+
+module.exports = router
